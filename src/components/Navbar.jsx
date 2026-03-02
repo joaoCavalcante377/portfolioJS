1aec641478f93b2bb548681245f9602d487b6b84
joaoCavalcante377 <joaocavalcante377@gmail.com>
2026-03-01 23:22:09 -0300

    styled: ajustes na navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,32 @@
-import { Link } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
+import { useEffect, useRef } from "react"
 import "./navbar.css"
 
-function Navbar(){
+function Navbar() {
+    const location = useLocation()
+    const sliderRef = useRef(null)
+    const navRef = useRef(null)
+
+    useEffect(() => {
+        const active = navRef.current?.querySelector(".active")
+        const slider = sliderRef.current
+        if (active && slider) {
+            slider.style.width = `${active.offsetWidth}px`
+            slider.style.left = `${active.offsetLeft}px`
+        }
+    }, [location])
+
     return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/sobre">Sobre</Link>
-        <Link to="/projetos">Projetos</Link>
+        <div className="links" ref={navRef}>
+            <div className="slider" ref={sliderRef}></div>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/sobre">Sobre</NavLink>
+            <NavLink to="/projetos">Projetos</NavLink>
+        </div>
     </nav>
     )
+ 
 }
 
 export default Navbar
